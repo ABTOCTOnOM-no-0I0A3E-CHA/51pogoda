94d8c50480146c11aebcab25893431265b157886
ABTOCTOnOM-no-0I0A3E-CHA <l4pk@icloud.com>
2026-07-05 14:14:54 +0300

    feat: full privacy policy with IP credentials, noindex on policy page

--- app/robots.ts
+++ app/robots.ts
@@ -9,7 +9,9 @@ export default function robots(): MetadataRoute.Robots {
     };
   }
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: [
+      { userAgent: "*", allow: "/", disallow: ["/politika"] },
+    ],
     sitemap: `${SITE.url}/sitemap.xml`,
     host: SITE.url,
   };
